cdaa41c2b9d6a492a5327d193b849cea5898d973
vagrant <vagrant@homestead>
2019-10-06 07:41:25 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,10 @@
 <?php
+
+/*
+ * 将当前请求的路由名称转换为CSS类名称
+ */
+function route_class()
+{
+    //使用 - 替换 .  Route::currentRouteName()  获取当前请求的路由名称
+    return str_replace('.','-',Route::currentRouteName());
+}
